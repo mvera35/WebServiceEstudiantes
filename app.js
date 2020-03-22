6ebd7bea9cc8adb6e813bee7e924bba1cc570f19
maxv3494 <mvera35@uabc.edu.mx>
2020-03-21 19:23:27 -0700

    cambios mínimos en los modelos, conexión con la base de datos y creación de tablas

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log();
+const modeloEstudiante = require('./models/estudiantesModel.js');
+const modeloCurso = require('./models/cursosModel.js');
+//En caso que las tablas no existan, se crean dentro de la base de datos
+//base de datos: escolar 
+modeloEstudiante.Estudiante.sync({force: false});
+modeloCurso.Curso.sync({force: false});
--- models/estudiantesModel.js
+++ models/estudiantesModel.js
@@ -1,14 +1,21 @@
 const Sequelize = require('sequelize');
-
+//Mediante Sequelize podremos conectarnos a la base de datos
 const sequelize = new Sequelize(
-  'escolar',
-  'backenduser',
-  'superpassword',
+  'escolar',//nombre de la base de datos
+  'backenduser',//usuario
+  'superpassword',//contraseña
   {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',//Indicamos que el dialecto es mysql
+    define:{
+      /*Aquí eliminamos unos valores por defecto en el modelo Curso,
+      es decir, evitamos que aparezcan campos no indicados a la hora
+      de crear una tabla*/
+      timestamps: false
+    }
   });
 
+//Definimos los campos del estudiante
 const Estudiante =
 sequelize.define('estudiante',{
   //atributos
@@ -39,7 +46,11 @@ sequelize.define('estudiante',{
     defaultValue: 0
   }
 });
+//Aquí se declaran los campos del modelo
+class e extends Estudiante {}
+e.init({},{sequelize});
 
+//Exportamos las constantes
 exports.Estudiante = Estudiante;
 exports.Sequelize = Sequelize;
 exports.sequelize = sequelize;
